0dfe7df6bcd0ff23e187a36d3456514eab725ef7
daniel <delmontejohndaniel1415@gmail.com>
2023-03-27 20:54:50 +0800

    added CompanyTab and TabPane for TripRates

---
+++ src/components/TabPane.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'TabPane',
+  props: ['classes', 'id']
+};
+</script>
+
+<template>
+  <div
+    class="tab-pane fade"
+    :class="classes"
+    :id="id"
+    role="tabpanel"
+    :aria-labelledby="id + '-tab'"
+    tabindex="0"
+  >
+    <slot></slot>
+  </div>
+</template>
