47d63305bda4827aefa3774e1efd5e61078ca83c
daniel <delmontejohndaniel1415@gmail.com>
2023-03-30 20:28:07 +0800

    added bg-color for cardDashboard

--- src/components/CardDashboard.vue
+++ src/components/CardDashboard.vue
@@ -1,12 +1,14 @@
 <script>
 export default {
-  name: 'CardDashboard'
+  name: 'CardDashboard',
+  props: ['classes']
 }
 </script>
 
 <template>
-  <div class="card w-45">
-    <div class="card-body text-center">
+  <div class="card w-45 position-relative">
+    <div class="overlay" :class="classes"></div>
+    <div class="card-body text-center d-flex flex-column justify-content-between">
       <slot name="title"></slot>
       <slot name="description"></slot>
       <slot name="link"></slot>
@@ -17,5 +19,14 @@ export default {
 <style scoped>
 .w-45 {
   width: 45%;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0.3;
 }
 </style>
--- src/model/CardsData.js
+++ src/model/CardsData.js
@@ -1,27 +1,27 @@
 const cardsData = [
   {
     title: 'Employees',
-    description: 'Description about Employee',
+    description: 'A portion of a company records that holds employees information.',
     path: '/employee',
   },
   {
     title: 'Clients',
-    description: 'Description about Clients',
+    description: 'Details regarding the clients associated with the company.',
     path: '/client',
   },
   {
     title: 'Trip Rates',
-    description: 'Description about Trip Rates',
+    description: 'Details about the prices charged for various sorts of trips.',
     path: '/triprates',
   },
   {
     title: 'Billing',
-    description: 'Description about Billing',
+    description: 'Billing information for services supplied to clients.',
     path: '/billing',
   },
   {
     title: 'Payroll',
-    description: 'Description about Payroll',
+    description: 'Information linked to the salary of both internal personnel and sub-contractors.',
     path: '/payroll',
   }
 ];
