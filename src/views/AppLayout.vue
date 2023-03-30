68b26d2cfff5472804db0a1d2bcb965e62f1c88a
daniel <delmontejohndaniel1415@gmail.com>
2023-03-30 19:42:28 +0800

    temporary login component

--- src/views/AppLayout.vue
+++ src/views/AppLayout.vue
@@ -1,19 +1,32 @@
 <script>
+import Login from './Login.vue';
 import Sidebar from '../components/Sidebar.vue';
 import Footer from '../components/Footer.vue';
 import { RouterView } from 'vue-router';
 
 export default {
   name: 'AppLayout',
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  },
   components: {
+    Login,
     Sidebar,
     Footer
-  }
+  },
+  emits: ['on-login']
 }
 </script>
 
 <template>
-  <div class="container-fluid">
+
+  <div v-if="!isLoggedIn">
+    <Login @on-login="isLoggedIn = true" />
+  </div>
+
+  <div v-else class="container-fluid">
     <div class="row">
       <div class="col-3 p-0 min-vh-100">
         <Sidebar />
