9805a7fd26aaa3fac4f57318cb911865dd633e36
Jolayus <juliuscabangon30@gmail.com>
2023-05-05 00:30:59 +0800

    Archives and Minor Fixes??
    - Added --emptyOutDir in vite build to clean server public folder
    - Added Archive Icon
    - Added Archives to Router

---
+++ client/src/views/ArchivedClient.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    name: 'ArchivedClient',
+    components: {
+
+    }
+};
+</script>
+
+<template>
+    <h1><center>Archived Clients</center></h1>
+</template>
+
+<style>
+
+</style>
---
+++ client/src/views/ArchivedEmployee.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    name: 'ArchivedEmployee',
+    components: {
+
+    }
+};
+</script>
+
+<template>
+    <h1><center>Archived Employees</center></h1>
+</template>
+
+<style>
+
+</style>
