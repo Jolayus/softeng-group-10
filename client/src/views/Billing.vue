104ebfd9be7e9440daaf777eb2ee7886e8f94e0e
daniel <delmontejohndaniel1415@gmail.com>
2023-05-05 11:25:57 +0800

    implement design for billing

--- client/src/views/Billing.vue
+++ client/src/views/Billing.vue
@@ -11,7 +11,7 @@ export default {
   data() {
     return {
       clients: getClientsModel()
-    }
+    };
   },
   components: {
     CompanyTab,
@@ -25,8 +25,8 @@ export default {
 <template>
   <h1>Clients - Billing</h1>
   <ul class="nav nav-pills mb-3 gap-2" id="billing-tab" role="tablist">
-    <CompanyTab  
-      v-for="client in clients" 
+    <CompanyTab
+      v-for="client in clients"
       :classes="client === clients[0] ? 'active' : ''"
       :id="'billing-' + client.company_name.split(' ').join('') + '-tab'"
       :target="'#billing-' + client.id"
@@ -36,29 +36,65 @@ export default {
     </CompanyTab>
   </ul>
   <div class="tab-content" id="pills-tabContent">
-    <TabPane 
+    <TabPane
       v-for="client in clients"
       :classes="client === clients[0] ? 'active show' : ''"
       :id="'billing-' + client.id"
     >
-    {{ client.company_name }}
-    <table class="table">
-        <thead class="tbl-header-billing text-light rounded">
-          <tr>
-            <th scope="col" rowspan="2">
-              <h1>GREENCROSS INCORPORATED</h1>
-              <p>14/F COMMON GOAL TOWER FINANCE COR INDUSTRY STS. MADRIGAL BUSINESS PARK AYALA ABALANG 1780, CITY OF MUNTINLUPA, FOURTH DISTRICT 
+
+      <main class="billing text-white">
+        <header
+          class="header-billing d-flex flex-row justify-content-center align-items-center"
+        >
+          <section class="w-75">
+            <div class="border-right border-btm">
+              <h1>{{ client.company_name }}</h1>
+              <p class="client_address">
+                14/F COMMON GOAL TOWER FINANCE COR INDUSTRY STS. MADRIGAL
+                BUSINESS PARK AYALA ABALANG 1780, CITY OF MUNTINLUPA, FOURTH
+                DISTRICT
               </p>
-            </th>
-            <th scope="col" class="min-width-100px" rowspan="2">
-              <p>02-Feb-22</p>
-              <p>30 DAYS</p>
-            </th>
-          </tr>
-        </thead>
-        <tbody class="table-group-divider">
-        </tbody>
-      </table>
+            </div>
+            <h3 class="border-right mb-0">{{ client.company_name }}</h3>
+          </section>
+          <section class="d-flex flex-column justify-content-center h-100 w-25">
+            <p class="billing_date m-0 p-2 border-y">02-Feb-22</p>
+            <p class="billing_remaining_days m-0 p-2 border-btm">30 days</p>
+            <p class="billing_id m-0 p-2 border-btm">000-416-871-00000</p>
+          </section>
+        </header>
+        <div class="separator border-y p-3"></div>
+
+        <main class="text-black d-flex flex-column">
+
+          <section class="d-flex w-100 height-50 border-btm">
+            <div class="d-flex justify-content-around align-items-center w-75">
+              <p class="mb-0 width-50">1</p>
+              <p class="mb-0">02-Feb-23</p>
+              <p class="mb-0">Shipment No.: 4610125213</p>
+            </div>
+            <p class="w-25 mb-0 d-flex justify-content-center align-items-center">4,000.00</p>
+          </section>
+
+          <section class="d-flex w-100 height-50 border-btm">
+            <div class="d-flex justify-content-around align-items-center w-75">
+              <p class="mb-0 width-50"></p>
+              <p class="mb-0"></p>
+              <p class="mb-0">SPO No.: 1500115216</p>
+            </div>
+            <p class="w-25 mb-0 d-flex justify-content-center align-items-center"></p>
+          </section>
+
+          <section class="d-flex w-100 height-50">
+            <div class="d-flex justify-content-around align-items-center w-75">
+              <p class="mb-0 width-50"></p>
+              <p class="mb-0"></p>
+              <p class="mb-0"></p>
+            </div>
+            <p class="w-25 mb-0 d-flex justify-content-center align-items-center fw-bold">4,000.00</p>
+          </section>
+        </main>
+      </main>
     </TabPane>
   </div>
   <FloatingActionButtonVue />
@@ -69,7 +105,56 @@ export default {
   min-width: 100px;
 }
 
-.tbl-header-billing {
+.header-billing,
+.separator {
   background-color: #4c7273;
+}
+
+.billing {
+  border: 5px solid black;
+}
+
+.border-left {
+  border-left: 5px solid black;
+}
+
+.border-right {
+  border-right: 5px solid black;
+}
+
+.border-top {
+  border-top: 5px solid black;
+}
+
+.border-btm {
+  border-bottom: 5px solid black;
+}
+
+.border-y {
+  border-top: 5px solid black;
+  border-bottom: 5px solid black;
+}
+
+.border-x {
+  border-right: 5px solid black;
+  border-left: 5px solid black;
+}
+
+.min-height-50 {
+  min-height: 50px;
+}
+
+.center-x-y {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.width-50 {
+  width: 50px;
+}
+
+.height-50 {
+  height: 50px;
 }
 </style>
