a196e3b2a022071dfef404721b8c32b4616f03e5
Jolayus <juliuscabangon30@gmail.com>
2023-05-06 20:38:55 +0800

    Added Archive Tables

--- client/src/views/ArchivedClient.vue
+++ client/src/views/ArchivedClient.vue
@@ -1,17 +1,93 @@
 <script>
+import SearchIcon from '../components/Icons/SearchIcon.vue';
+import { getClientsModel } from '../models/client.model';
 
 export default {
     name: 'ArchivedClient',
     components: {
-
+        SearchIcon,
+    },
+    data() {
+        return {
+            clientsModel: getClientsModel(),
+            selectedClient: null,
+            currentClientId: 0,
+            searchInput: '',
+        };
+    },
+    computed: {
+        filteredClient() {
+            return this.clientsModel.filter((client) =>
+                client.company_name
+                    .toLowerCase()
+                    .includes(this.searchInput.toLowerCase())
+            );
+        },
+    },
+    mounted() {
+        this.currentClientId = this.clientsModel.length;
     }
 };
 </script>
 
 <template>
-    <h1><center>Archived Clients</center></h1>
+    <div class="d-flex flex-column justify-content-between h-100">
+        <header class="mb-5">
+          <h1>Archived Clients</h1>
+        </header>
+        <main class="container flex-grow-1">
+          <div class="d-flex justify-content-between mb-4" style="max-height: 35px">
+            <div class="input-group mb-3 h-100 align-items-center gap-2">
+              <label for="user-input">Search:</label>
+              <input
+                v-model="searchInput"
+                type="text"
+                class="form-control"
+                placeholder="Client's name"
+                aria-label="Recipient's username"
+                id="user-input"
+                aria-describedby="basic-addon2"
+              />
+            </div>
+          </div>
+          <table class="table">
+            <thead class="tbl-header text-light rounded">
+              <tr>
+                <th scope="col">Company Name</th>
+                <th scope="col">Contact Person</th>
+                <th scope="col">Contact Number</th>
+                <th scope="col">Address</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody class="table-group-divider">
+              <tr v-for="client in filteredClient" :key="client.id">
+                <th scope="row">{{ client.company_name }}</th>
+                <td>{{ client.contact_person }}</td>
+                <td>{{ client.contact_number }}</td>
+                <td>{{ client.address }}</td>
+                <td>
+
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
+      </div>
 </template>
 
-<style>
+<style scoped>
+.input-group {
+  width: 45%;
+}
 
+.modal-body label {
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #86b9b0;
+}
+
+.modal-footer button {
+  background-color: #4c7273;
+}
 </style>
--- client/src/views/ArchivedEmployee.vue
+++ client/src/views/ArchivedEmployee.vue
@@ -1,17 +1,90 @@
 <script>
+import SearchIcon from '../components/Icons/SearchIcon.vue';
+import { getEmployeesModel } from '../models/employees.model';
 
 export default {
     name: 'ArchivedEmployee',
     components: {
+        SearchIcon,
+    },
+    data() {
+        return {
+            employeesModel: getEmployeesModel(),
+            selectedEmployee: null,
+            searchInput: '',
+        };
+    }, 
+    computed: {
+        filteredEmployees() {
+            const employees = this.employeesModel.filter((employee) =>
+                employee.name.toLowerCase().includes(this.searchInput.toLowerCase())
+            );
 
+            return employees;
+        },
     }
 };
 </script>
 
 <template>
-    <h1><center>Archived Employees</center></h1>
+    <div class="d-flex flex-column justify-content-between h-100">
+        <header class="mb-5">
+          <h1>Archived Employees</h1>
+        </header>
+        <main class="container flex-grow-1">
+          <div class="d-flex justify-content-between mb-4" style="max-height: 35px">
+            <div class="input-group mb-3 h-100 align-items-center gap-2">
+              <label for="user-input">Search:</label>
+              <input
+                v-model="searchInput"
+                type="text"
+                class="form-control"
+                placeholder="Employee's name"
+                aria-label="Recipient's username"
+                id="user-input"
+                aria-describedby="basic-addon2"
+              />
+            </div>
+          </div>
+          <table class="table">
+            <thead class="tbl-header text-light rounded">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Role</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone #</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody class="table-group-divider">
+              <tr v-for="employee in filteredEmployees" :key="employee.id">
+                <th scope="row">{{ employee.name }}</th>
+                <td>{{ employee.role }}</td>
+                <td>{{ employee.email }}</td>
+                <td>{{ employee.contact_number }}</td>
+                <td>
+
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
+      </div>
 </template>
 
-<style>
+<style scoped>
 
+.input-group {
+  width: 45%;
+}
+
+.modal-body label {
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #86b9b0;
+}
+
+.modal-footer button {
+  background-color: #4c7273;
+}
 </style>
