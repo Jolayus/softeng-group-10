a1f9b49c553a34b229cc41d58106ec26a638db44
Mirai <delmontejohndaniel1415@gmail.com>
2024-01-07 10:49:32 +0800

    added DELETE route for /externalSalaries

--- server/src/routes/externalSalaries/externalSalaries.router.js
+++ server/src/routes/externalSalaries/externalSalaries.router.js
@@ -3,7 +3,8 @@ const express = require('express');
 const {
   httpGetAllExternalSalaries,
   httpPostNewExternalSalary,
-  httpEditExternalSalary
+  httpEditExternalSalary,
+  httpDeleteExternalSalaries
 } = require('./externalSalaries.controller');
 
 const externalSalariesRouter = express.Router();
@@ -11,5 +12,6 @@ const externalSalariesRouter = express.Router();
 externalSalariesRouter.get('/', httpGetAllExternalSalaries);
 externalSalariesRouter.post('/', httpPostNewExternalSalary);
 externalSalariesRouter.patch('/', httpEditExternalSalary);
+externalSalariesRouter.delete('/', httpDeleteExternalSalaries);
 
 module.exports = externalSalariesRouter;
